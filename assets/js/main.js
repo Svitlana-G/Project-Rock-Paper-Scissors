e116a476f9dd8c0f21f0b8e85e876fed4ace92d1
Svitlana-G <svitlana.golubieva@gmail.com>
2021-05-25 21:56:37 +0200

    Process total score

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,88 +6,74 @@ let result = document.getElementById('result')
 let user = document.getElementById('user')
 let comp = document.getElementById('comp')
 
-// console.log(user)
+let rock = document.getElementById('rock')
+let scissors = document.getElementById('scissors')
+let paper = document.getElementById('paper')
+
 let userRes = 0;
 let compRes = 0;
+let roundCurrent = 0;
 
+//User hat einen Stein gewählt
 function scoreRock() {
-    console.log("User: Rock")
-
     let compZuf = computer()
-    console.log('Computer:' + compZuf)
 
     if (compZuf == 0) {
-        console.log('Unentschieden')
         result.innerHTML = 'You both chose Rock'
     }
     else if (compZuf == 1) {
-        console.log("Gewinn")
-        result.innerHTML = 'Rock(user) Scissors(comp). You win!'
+        result.innerHTML = 'Rock <sup>(user)</sup> Scissors <sup>(comp)</sup>. You win!'
         userRes++
-        console.log('user:' + userRes)
         user.innerHTML = userRes
     }
     else {
-        console.log("Niederlage")
-        result.innerHTML = 'Rock(user) Paper(comp). You lose!'
+        result.innerHTML = 'Rock <sup>(user)</sup> Paper <sup>(comp)</sup>. You lose!'
         compRes++
-        console.log('comp:' + compRes)
         comp.innerHTML = compRes
+    }
+    if (roundCurrent == roundValue) {
+        result.innerHTML = "Game over"
     }
 }
 
-
+// User hat die Schere gewählt
 function scoreScissors() {
-    console.log("User: Scissors")
-
     let compZuf = computer()
-    console.log('Computer:' + compZuf)
 
     if (compZuf == 1) {
-        console.log('Unentschieden')
         result.innerHTML = 'You both chose Scissors'
     }
     else if (compZuf == 2) {
-        console.log("Gewinn")
-        result.innerHTML = 'Scissors(user) Paper(comp). You win!'
+        result.innerHTML = 'Scissors <sup>(user)</sup> Paper <sup>(comp)</sup>. You win!'
         userRes++
-        console.log('user:' + userRes)
         user.innerHTML = userRes
     }
     else {
-        console.log("Niederlage")
-        result.innerHTML = 'Scissors(user) Rock(comp). You lose!'
+        result.innerHTML = 'Scissors <sup>(user)</sup> Rock <sup>(comp)</sup>. You lose!'
         compRes++
-        console.log('comp:' + compRes)
         comp.innerHTML = compRes
     }
 }
-
+// User hat Papier gewählt
 function scorePaper() {
-    console.log("User: Paper")
     let compZuf = computer()
-    console.log('Computer:' + compZuf)
 
     if (compZuf == 2) {
-        console.log('Unentschieden')
         result.innerHTML = 'You both chose Paper'
     }
     else if (compZuf == 0) {
-        console.log("Gewinn")
-        result.innerHTML = 'Paper(user) Rock(comp). You win!'
+        result.innerHTML = 'Paper <sup>(user)</sup> Rock <sup>(comp)</sup>. You win!'
         userRes++
-        console.log('user:' + userRes)
         user.innerHTML = userRes
     }
     else {
-        console.log("Niederlage")
-        result.innerHTML = 'Paper(user) Scissors(comp). You lose!'
+        result.innerHTML = 'Paper <sup>(user)</sup> Scissors <sup>(comp)</sup>. You lose!'
         compRes++
-        console.log('comp:' + compRes)
         comp.innerHTML = compRes
     }
 }
 
+// Zufällige Wahl von Computer
 function computer() {
     return Math.floor(Math.random() * 3) // Zahl zwischen 0 - 2
     //0 - Rock
@@ -95,9 +81,67 @@ function computer() {
     //2 - Paper
 }
 
+// Neues Spiel
 function restart() {
     let userRes = 0;
     let compRes = 0;
     comp.innerHTML = compRes;
     user.innerHTML = userRes
+}
+
+// Anzahl von Runde
+let roundValue
+function rounds() {
+    let round = document.getElementsByName('rounds')
+    for (let i = 0; i < round.length; i++) {
+        if (round[i].checked) {
+            roundValue = round[i].value
+        }
+    }
+}
+// Zeigt laufendes Ergebnis
+function wechseln() {
+    let roundCur = document.getElementById('roundCurrent')
+    roundCur.classList.toggle('hide')
+    let roundToggle = document.getElementById('round')
+    roundToggle.classList.toggle('hide')
+}
+// Hauptfunktion
+function spiel(auswahl) {
+    rounds()
+
+    if (roundCurrent == 0) {
+        wechseln()
+        document.getElementById('total').innerHTML = roundValue
+    }
+
+    roundCurrent = roundCurrent + 1
+
+    if (roundCurrent <= roundValue) {
+        document.getElementById('current').innerHTML = roundCurrent
+
+        if (auswahl == 'rock') {
+            scoreRock()
+        }
+        else if (auswahl == 'scissors') {
+            scoreScissors()
+        }
+        else if (auswahl == 'paper') {
+            scorePaper()
+        }
+        else {
+            console.log('Fehler')
+        }
+    }
+    if (roundCurrent == roundValue) {
+
+        if (userRes > compRes) {
+            return result.innerHTML = " The user wins"
+        }
+        else if (userRes == compRes) {
+            return result.innerHTML = "Draw"
+        }
+        else
+            return result.innerHTML = " Computer wins"
+    }
 }
